84a4285c4bce17ee1e24e62dfa10c22b646bc012
jp calvo <calvojp92@gmail.com>
2024-05-16 16:56:50 +0800

    init range calendar test

--- src/lib/utils/create-range-calendar.spec.ts
+++ src/lib/utils/create-range-calendar.spec.ts
@@ -1,1 +1,26 @@
-test.todo('createRangeCalendar');
+import { createRangeCalendar } from './create-range-calendar.js';
+
+test('createRangeCalendar', () => {
+  const calendars = createRangeCalendar(new Date());
+
+  expect(calendars).toEqual(expect.any(Array));
+  expect(calendars.length).toBe(2);
+  expect(calendars[0]).toEqual(
+    expect.objectContaining({
+      year: expect.any(Number),
+      month: expect.any(String),
+      weeks: expect.any(Array),
+      weekdays: expect.any(Array),
+      days: expect.any(Array),
+    }),
+  );
+  expect(calendars[1]).toEqual(
+    expect.objectContaining({
+      year: expect.any(Number),
+      month: expect.any(String),
+      weeks: expect.any(Array),
+      weekdays: expect.any(Array),
+      days: expect.any(Array),
+    }),
+  );
+});
