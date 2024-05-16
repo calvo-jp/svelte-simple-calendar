bbd8c621a3efcdfbe3d1abdbaaefc702eeb90fa2
jp calvo <calvojp92@gmail.com>
2024-05-16 15:51:02 +0800

    add test

---
+++ src/lib/compare-asc.spec.ts
@@ -0,0 +1,13 @@
+import { compareAsc } from './compare-asc.js';
+
+test('compareAsc', () => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  const dates = [today, yesterday, tomorrow];
+  dates.sort(compareAsc);
+  expect(dates).toEqual([yesterday, today, tomorrow]);
+});
---
+++ src/lib/is-same-month.spec.ts
@@ -0,0 +1,6 @@
+import { isSameMonth } from './is-same-month.js';
+
+test('isSameMonth', () => {
+  expect(isSameMonth(new Date(2021, 0, 1), new Date(2021, 0, 25))).toBe(true);
+  expect(isSameMonth(new Date(2021, 0, 1), new Date(2021, 1, 25))).toBe(false);
+});
