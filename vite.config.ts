8b08316280f0f49fb67ae2dbf311e1858eaa9eed
jp calvo <calvojp92@gmail.com>
2024-05-22 12:03:17 +0800

    add floating ui dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import packageJson from './package.json';
 
 export default defineConfig({
   plugins: [sveltekit(), enhancedImages()],
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      external: Object.keys(packageJson.dependencies),
+    },
   },
   test: {
     watch: false,
